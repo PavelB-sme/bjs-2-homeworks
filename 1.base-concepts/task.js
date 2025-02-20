dd3acd82bfc4dc78a3a933b7c8cf955d849af6ee
Pavel_B <bondarenko.sme@gmail.com>
2025-02-20 22:48:50 +0500

    change base-concept

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,15 +3,13 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d < 0) {
-    return arr;
-  } else if (d === 0) {
-    let x = -b / (2 * a);
-    arr.push(x);
-  } else {
+  if (d > 0) {
     let x1 = (-b + Math.sqrt(d)) / (2 * a);
     let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
   }
 
   return arr;
@@ -22,14 +20,10 @@ console.log(solveEquation(1, 5, 4));
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   percent = percent / 100 / 12;
   let bodyAmount = amount - contribution;
-  let payment =
-    bodyAmount * (percent + percent / ((1 + percent) ** countMonths - 1));
+  let payment = bodyAmount * (percent + percent / ((1 + percent) ** countMonths - 1));
   let totalMortgage = payment * countMonths;
-  if (typeof totalMortgage === 'number' && !isNaN(totalMortgage)) {
-    return +totalMortgage.toFixed(2);
-  } else {
-    return false;
-  }
+
+  return +totalMortgage.toFixed(2);
 }
 
-console.log(calculateTotalMortgage(15, 0, 10000, 36));
+console.log(calculateTotalMortgage(15, 0, '15000', 36));
