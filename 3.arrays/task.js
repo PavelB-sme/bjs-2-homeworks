8321da7bdfface010854cfccdeb2ee9385788e3f
Pavel_B <bondarenko.sme@gmail.com>
2025-02-28 15:45:03 +0500

    change Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+  const check = arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
+
+  return check;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.
+  filter(user => user.gender === gender).
+  map(user => user.age).
+  reduce((average, userAge, index, array) => average + userAge / array.length, 0);
 }
